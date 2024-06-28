f6df8b5d81669f161aec7477075a5562f0de29f3
ngreen64 <ng.x64.me@gmail.com>
2024-06-28 13:18:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,4 +10,10 @@ echo "<br>";
 echo "Running on backend IP address : ";
   $ip_addr = $_SERVER['SERVER_ADDR'];
   echo "\n".$ip_addr;
+echo "<br>";
+
+echo "My hostname is : ";
+$myfile = fopen("/etc/hostname", "r") or die("Unable to open file!");
+echo fread($myfile,filesize("/etc/hostname"));
+fclose($myfile);
 ?>
