dc8f5f41149e323c33d502288cdacdd05044044d
ngreen64 <ng.x64.me@gmail.com>
2024-06-28 13:24:23 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,18 +4,18 @@ echo "Welcome to Server : ";
   $protocol = (!empty($_SERVER['HTTPS']) && (strtolower($_SERVER['HTTPS']) == 'on' || $_SERVER['HTTPS'] == '1')) ? 'https://' : 'http://';
   $server = $_SERVER['SERVER_NAME'];
   $port = $_SERVER['SERVER_PORT'] ? ':'.$_SERVER['SERVER_PORT'] : '';
-  echo $protocol.$server.$port;
+  echo "<b>".$protocol.$server.$port.</b>;
 echo "<br>";
 echo "<br>";
 
 echo "Running on backend IP address : ";
   $ip_addr = $_SERVER['SERVER_ADDR'];
-  echo "\n".$ip_addr;
+  echo "<b>".$ip_addr."</b>;
 echo "<br>";
 echo "<br>";
 
 echo "My hostname is : ";
 $myfile = fopen("/etc/hostname", "r") or die("Unable to open file!");
-echo fread($myfile,filesize("/etc/hostname"));
+echo "<b>".fread($myfile,filesize("/etc/hostname"))."</b>;
 fclose($myfile);
 ?>
